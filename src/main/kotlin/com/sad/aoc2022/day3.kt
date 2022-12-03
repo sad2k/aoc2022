dbc73f299961cf910f9d852982d3519eb5f4d578
Sergey Dubakov <serge.dubakov@gmail.com>
2022-12-03 20:24:59 +0000

    Day 3 part 2

--- src/main/kotlin/com/sad/aoc2022/day3.kt
+++ src/main/kotlin/com/sad/aoc2022/day3.kt
@@ -1,18 +1,24 @@
 package com.sad.aoc2022
 
+private fun getPriority(item: Char) = if (item.isLowerCase()) {
+    item.code - 'a'.code + 1
+} else {
+    item.code - 'A'.code + 27
+}
+
 fun main() {
-    val input = loadFromResources("day3.txt").readLines().map {
+    val input = loadFromResources("day3.txt").readLines()
+
+    // part 1
+    println(input.map {
         val halfLength = it.length / 2
         it.substring(0, halfLength) to it.substring(halfLength)
     }
+        .map { (fst, snd) -> (fst.toSet() intersect snd.toSet()).first() }
+        .map(::getPriority).sum())
 
-    // part 1
-    println(input.map { (fst, snd) -> (fst.toSet() intersect snd.toSet()).first() }
-        .map {
-            if (it.isLowerCase()) {
-                it.code - 'a'.code + 1
-            } else {
-                it.code - 'A'.code + 27
-            }
-        }.sum())
+    // part 2
+    println(input.chunked(3).map {
+        (it[0].toSet() intersect it[1].toSet()) intersect it[2].toSet()
+    }.map { getPriority(it.first()) }.sum() )
 }
