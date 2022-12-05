67c6a1b98510e60c76a7d9501d21013071840930
Sergey Dubakov <serge.dubakov@gmail.com>
2022-12-05 15:53:06 +0000

    Day 5 refactoring

--- src/main/kotlin/com/sad/aoc2022/day5.kt
+++ src/main/kotlin/com/sad/aoc2022/day5.kt
@@ -2,8 +2,6 @@ package com.sad.aoc2022
 
 fun main() {
     val input = loadFromResources("day5.txt").readLines().splitWhen { it.isBlank() }
-
-    // part 1
     val stackLines = input[0].take(input[0].size - 1)
     val stackDef = input[0][input[0].size - 1]
     val stackIdx = mutableMapOf<Int, Int>()
@@ -12,7 +10,7 @@ fun main() {
             stackIdx[idx] = v.digitToInt()
         }
     }
-    val stacks = MutableList(stackIdx.size) { mutableListOf<Char>() }
+    val stacks = List(stackIdx.size) { mutableListOf<Char>() }
     stackLines.forEach {
         it.forEachIndexed { idx, v ->
             if (v.isLetter()) {
@@ -24,10 +22,15 @@ fun main() {
         val spl = it.split(" ")
         Triple(spl[1].toInt(), spl[3].toInt(), spl[5].toInt())
     }
+
+    // part 1
+    val target = stacks.map { it.toMutableList() }
     instructions.forEach { (num, from, to) ->
         for (i in 0 until num) {
-            stacks[to - 1].add(0, stacks[from - 1].removeFirst())
+            target[to - 1].add(0, target[from - 1].removeFirst())
         }
     }
-    println(stacks.map { it[0] }.joinToString(separator = ""))
+    println(target.map { it[0] }.joinToString(separator = ""))
+
+    // part 2
 }
