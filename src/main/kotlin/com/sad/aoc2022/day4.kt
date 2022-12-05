a2e50cb55812b8037c63e12cea6c2dea98ef1b61
Sergey Dubakov <serge.dubakov@gmail.com>
2022-12-05 14:35:20 +0000

    Day 4 part 2

--- src/main/kotlin/com/sad/aoc2022/day4.kt
+++ src/main/kotlin/com/sad/aoc2022/day4.kt
@@ -3,6 +3,10 @@ package com.sad.aoc2022
 private fun contains(bigger: List<Int>, smaller: List<Int>): Boolean =
     smaller[0] >= bigger[0] && smaller[1] <= bigger[1]
 
+private fun overlaps(fst: List<Int>, snd: List<Int>): Boolean =
+    (fst[0] >= snd[0] && fst[0] <= snd[1]) ||
+            (fst[1] >= snd[0] && fst[1] <= snd[1])
+
 fun main() {
     val input = loadFromResources("day4.txt").readLines().map {
         it.split(",").map { s ->
@@ -15,4 +19,8 @@ fun main() {
         (fst, snd) -> contains(fst, snd) || contains(snd, fst)
     }.count { it })
 
+    // part 2
+    println(input.map {
+            (fst, snd) -> overlaps(fst, snd) || overlaps(snd, fst)
+    }.count { it })
 }
