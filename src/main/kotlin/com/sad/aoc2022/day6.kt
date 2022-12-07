5c4ed0008406155f451616de174b53fbec98128c
Sergey Dubakov <serge.dubakov@gmail.com>
2022-12-07 10:15:08 +0000

    Day 6 part 2

--- src/main/kotlin/com/sad/aoc2022/day6.kt
+++ src/main/kotlin/com/sad/aoc2022/day6.kt
@@ -3,13 +3,20 @@ package com.sad.aoc2022
 fun main() {
     val input = loadFromResources("day6.txt").readFirstLine()
 
-    // part 1
-    val windowed = input.windowed(size = 4, step = 1)
-    for (i in windowed.indices) {
-        val w = windowed[i]
-        if (w.toSet().size == 4) {
-            println(i + 4)
-            break
+    fun solve(size: Int): Int {
+        val windowed = input.windowed(size = size, step = 1)
+        for (i in windowed.indices) {
+            val w = windowed[i]
+            if (w.toSet().size == size) {
+                return (i + size)
+            }
         }
+        throw IllegalArgumentException()
     }
+
+    // part 1
+    println(solve(4))
+
+    // part 2
+    println(solve(14))
 }
