1c20f423c36fbabb73ff8426383f3348c915f7ba
Sergey Dubakov <serge.dubakov@gmail.com>
2022-12-10 23:11:06 +0000

    day 10 part 2

--- src/main/kotlin/com/sad/aoc2022/day10.kt
+++ src/main/kotlin/com/sad/aoc2022/day10.kt
@@ -1,9 +1,6 @@
 package com.sad.aoc2022
 
-fun main() {
-    val input = loadFromResources("day10.txt").readLines()
-
-    // part 1
+private fun part1(input: List<String>) {
     var cycle = 1
     var x = 1
     var result = 0L
@@ -33,3 +30,53 @@ fun main() {
     }
     println(result)
 }
+
+private fun part2(input: List<String>) {
+    var cycle = 1
+    var x = 1
+    var sb = StringBuilder()
+    var xpos = 0
+    for (line in input) {
+        val spl = line.split(" ")
+        var cyclesToWait = 0
+        var add = 0
+        when (spl[0]) {
+            "noop" -> {
+                cyclesToWait = 1
+                add = 0
+            }
+            "addx" -> {
+                cyclesToWait = 2
+                add = spl[1].toInt()
+            }
+            else -> throw IllegalArgumentException()
+        }
+        for (i in 1..cyclesToWait) {
+            val sprite = listOf(x - 1, x, x + 1)
+            if (xpos in sprite) {
+                sb.append("#")
+            } else {
+                sb.append(".")
+            }
+            cycle++
+            xpos++
+            if (xpos == 40) {
+                xpos = 0
+                sb.append("\n")
+            }
+        }
+        x += add
+    }
+    println(sb)
+}
+
+fun main() {
+    val input = loadFromResources("day10.txt").readLines()
+
+    // part 1
+    part1(input)
+
+    // part 2
+    part2(input)
+}
+
