79ef4ca5d3d9da1d3d5434bc3ef073121b06c233
Sergey Dubakov <serge.dubakov@gmail.com>
2022-12-15 22:00:20 +0000

    day 15 part 1 refactoring

--- src/main/kotlin/com/sad/aoc2022/day15.kt
+++ src/main/kotlin/com/sad/aoc2022/day15.kt
@@ -17,12 +17,6 @@ private fun toNumber(s: String): Int {
 private fun distance(p1: Pair<Int, Int>, p2: Pair<Int, Int>) =
     abs(p1.first - p2.first) + abs(p1.second - p2.second)
 
-//private fun printGrid(grid: List<MutableList<Char>>) {
-//    for (gridRow in grid) {
-//        println(gridRow.joinToString(separator = ""))
-//    }
-//}
-
 fun drawArea(grid: MutableList<Char>, sensor: Pair<Int, Int>, distance: Int, minx: Int, targety: Int) {
     if (targety in sensor.second - distance..sensor.second + distance) {
         for (x in sensor.first - distance..sensor.first + distance) {
@@ -31,6 +25,31 @@ fun drawArea(grid: MutableList<Char>, sensor: Pair<Int, Int>, distance: Int, min
             }
         }
     }
+}
+
+private fun part1(
+    maxx: Int,
+    minx: Int,
+    sensors: List<Pair<Int, Int>>,
+    beacons: List<Pair<Int, Int>>
+) {
+    val targety = 2000000
+
+    val grid = MutableList(maxx - minx + 1) { '.' }
+    for ((sensor, beacon) in sensors zip beacons) {
+        if (sensor.second == targety) {
+            grid[sensor.first - minx] = 'S'
+        }
+        if (beacon.second == targety) {
+            grid[beacon.first - minx] = 'B'
+        }
+    }
+    for ((sensor, beacon) in sensors zip beacons) {
+        val distance = distance(sensor, beacon)
+        drawArea(grid, sensor, distance, minx, targety)
+    }
+
+    println(grid.count { it == '#' })
 }
 
 fun main() {
@@ -69,23 +88,8 @@ fun main() {
     }
     println("minx: ${minx} maxx: ${maxx} miny: ${miny} maxy: ${maxy}")
 
-    val targety = 2000000
-
-    val grid = MutableList(maxx - minx + 1) { '.' }
-    for ((sensor, beacon) in sensors zip beacons) {
-        if (sensor.second == targety) {
-            grid[sensor.first - minx] = 'S'
-        }
-        if (beacon.second == targety) {
-            grid[beacon.first - minx] = 'B'
-        }
-    }
-    for ((sensor, beacon) in sensors zip beacons) {
-        val distance = distance(sensor, beacon)
-        drawArea(grid, sensor, distance, minx, targety)
-    }
-
-    println(grid.count { it == '#' })
+    part1(maxx, minx, sensors, beacons)
 }
+
 
 
