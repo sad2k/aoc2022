2bc49eba4af48e38951b4490d7c2c645450cc3c5
Sergey Dubakov <serge.dubakov@gmail.com>
2022-12-19 22:33:04 +0000

    day 18 part 2

--- src/main/kotlin/com/sad/aoc2022/day18.kt
+++ src/main/kotlin/com/sad/aoc2022/day18.kt
@@ -19,6 +19,12 @@ private fun countExposedSides(coord: Triple<Int, Int, Int>, cubes: Set<Triple<In
     }.count()
 }
 
+private fun countSidesExposedToOutside(coord: Triple<Int, Int, Int>, cubes: Set<Triple<Int, Int, Int>>, outsideCoords: Set<Triple<Int,Int,Int>>): Int {
+    return neighbours(coord).filter {
+        !cubes.contains(it) && outsideCoords.contains(it)
+    }.count()
+}
+
 fun main() {
     val input = loadFromResources("day18.txt").readLines().map {
         val l = it.split(",").map { s ->
@@ -31,4 +37,32 @@ fun main() {
     println(input.map {
         countExposedSides(it, input)
     }.sum())
+
+    // part 2
+    val maxx = input.maxOf { it.first } + 1
+    val maxy = input.maxOf { it.second } + 1
+    val maxz = input.maxOf { it.third } + 1
+    val queue = ArrayDeque<Triple<Int, Int, Int>>()
+    val visited = mutableSetOf<Triple<Int, Int, Int>>()
+    queue.add(Triple(maxx, maxy, maxz))
+    while (!queue.isEmpty()) {
+        val c = queue.removeFirst()
+        if (visited.contains(c)) {
+            continue
+        }
+        val neighbours = neighbours(c).filter {
+            it.first <= maxx && it.second <= maxy && it.third <= maxz &&
+                    it.first >= -1 && it.second >= -1 && it.third >= -1 &&
+                    !input.contains(it)
+        }
+        neighbours.forEach {
+            queue.add(it)
+        }
+        visited.add(c)
+    }
+//    println(visited)
+    println(input.map {
+        countSidesExposedToOutside(it, input, visited)
+    }.sum())
+
 }
