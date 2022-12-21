28fa1c684502740895dddb86013d966ad3f67b3b
Sergey Dubakov <serge.dubakov@gmail.com>
2022-12-21 10:35:36 +0000

    day 20 part 2

--- src/main/kotlin/com/sad/aoc2022/day20.kt
+++ src/main/kotlin/com/sad/aoc2022/day20.kt
@@ -50,6 +50,62 @@ fun part1(input: List<Pair<Int, Int>>) {
     }
 }
 
+fun part2(input: List<Pair<Int, Int>>) {
+    val list = input.map { (v, idx) ->
+        v.toLong() * 811589153 to idx
+    }.toMutableList()
+    val indexes = mutableMapOf<Int, Int>()
+    list.forEach { (_, i) ->
+        indexes[i] = i
+    }
+    println(list)
+    repeat(10) {
+        for (i in list.indices) {
+            val idx = indexes[i]!!
+            val (v, _) = list[idx]
+            var newPos = ((idx + v) % (list.size - 1)).toInt()
+            if (newPos < 0) {
+                newPos += list.size - 1
+            }
+//        println("${v} moves from ${idx} to ${newPos}")
+            if (idx != newPos) {
+                indexes[i] = newPos
+                if (newPos < idx) {
+                    for (j in newPos until idx) {
+                        val tpl = list[j]
+                        indexes[tpl.second] = j + 1
+                    }
+                } else {
+                    for (j in idx + 1..newPos) {
+                        val tpl = list[j]
+                        indexes[tpl.second] = j - 1
+                    }
+                }
+                list.removeAt(idx)
+                list.add(newPos, v to i)
+            }
+
+        }
+        println(list)
+        println(indexes)
+    }
+    for (i in list.indices) {
+        val (v, _) = list[i]
+        if (v == 0L) {
+            println("0 is at index ${i}")
+            val plus1000 = list[(i + 1000) % list.size]
+            println("0 + 1000 is $plus1000")
+            val plus2000 = list[(i + 2000) % list.size]
+            println("0 + 2000 is $plus2000")
+            val plus3000 = list[(i + 3000) % list.size]
+            println("0 + 3000 is $plus3000")
+            val s = plus1000.first + plus2000.first + plus3000.first
+            println("sum is ${s}")
+            break
+        }
+    }
+}
+
 fun main() {
     val input = loadFromResources("day20.txt").readLines().map(String::toInt).mapIndexed { idx, it ->
         it to idx
@@ -57,5 +113,8 @@ fun main() {
 
     // part 1
 //    part1(input)
+
+    // part 2
+    part2(input)
 }
 
