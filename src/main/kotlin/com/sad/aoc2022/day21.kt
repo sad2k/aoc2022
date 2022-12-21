0617ab6f283079ca2afd21fecc5d59d56f1a9fd2
Sergey Dubakov <serge.dubakov@gmail.com>
2022-12-21 21:10:01 +0000

    day 21 part 1

---
+++ src/main/kotlin/com/sad/aoc2022/day21.kt
@@ -0,0 +1,63 @@
+package com.sad.aoc2022
+
+import java.lang.IllegalArgumentException
+
+interface MathMonkey {
+    fun getNumber(): Long
+}
+
+data class NumberMonkey(private val number: Long) : MathMonkey {
+    override fun getNumber(): Long = number
+}
+
+abstract class CompositeMonkey(val monkey1: String, val monkey2: String, val cache: Map<String, MathMonkey>) :
+    MathMonkey
+
+class PlusMonkey(monkey1: String, monkey2: String, cache: Map<String, MathMonkey>) :
+    CompositeMonkey(monkey1, monkey2, cache) {
+    override fun getNumber(): Long = cache[monkey1]!!.getNumber() + cache[monkey2]!!.getNumber()
+}
+
+class MinusMonkey(monkey1: String, monkey2: String, cache: Map<String, MathMonkey>) :
+    CompositeMonkey(monkey1, monkey2, cache) {
+    override fun getNumber(): Long = cache[monkey1]!!.getNumber() - cache[monkey2]!!.getNumber()
+}
+
+class MultMonkey(monkey1: String, monkey2: String, cache: Map<String, MathMonkey>) :
+    CompositeMonkey(monkey1, monkey2, cache) {
+    override fun getNumber(): Long = cache[monkey1]!!.getNumber() * cache[monkey2]!!.getNumber()
+}
+
+class DivMonkey(monkey1: String, monkey2: String, cache: Map<String, MathMonkey>) :
+    CompositeMonkey(monkey1, monkey2, cache) {
+    override fun getNumber(): Long = cache[monkey1]!!.getNumber() / cache[monkey2]!!.getNumber()
+}
+
+fun main() {
+    val input = loadFromResources("day21.txt").readLines().map {
+        it.split(":\\s*".toRegex())
+    }
+    val cache = mutableMapOf<String, MathMonkey>()
+    input.forEach { (name, def) ->
+        val monkey =
+            if (def.all { it.isDigit() }) {
+                NumberMonkey(def.toLong())
+            } else if ('+' in def) {
+                val spl = def.split("\\s*\\+\\s*".toRegex())
+                PlusMonkey(spl[0], spl[1], cache)
+            } else if ('-' in def) {
+                val spl = def.split("\\s*\\-\\s*".toRegex())
+                MinusMonkey(spl[0], spl[1], cache)
+            } else if ('*' in def) {
+                val spl = def.split("\\s*\\*\\s*".toRegex())
+                MultMonkey(spl[0], spl[1], cache)
+            } else if ('/' in def) {
+                val spl = def.split("\\s*\\/\\s*".toRegex())
+                DivMonkey(spl[0], spl[1], cache)
+            } else {
+                throw IllegalArgumentException(def)
+            }
+        cache[name] = monkey
+    }
+    println(cache["root"]!!.getNumber())
+}
